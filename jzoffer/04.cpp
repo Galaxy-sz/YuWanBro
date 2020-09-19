f9483b0b1e7c5a110669207d5f069314d98da660
yeyuezhishui <925828909@qq.com>
2020-09-19 16:09:42 +0800

    add jzoffer 05 06

---
+++ jzoffer/04.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
+        if (matrix.size() == 0) return false;
+        if (matrix[0].size() == 0) return false;
+        int i = 0;
+        int j = matrix.size()-1;
+        while(i <= matrix[0].size()-1 && j >= 0){
+            if (matrix[j][i] == target) return true;
+            else if(matrix[j][i] < target){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return false;
+    }
+};
